6f6b6a6660d685daa11398f3072c7f5cb2d4b8fd
MasatoshiTada <rosso.e.nero84@gmail.com>
2015-11-09 07:06:09 +0900

    EJBに修正、DBをPostgreSQLに変更

--- src/main/java/com/example/dao/config/InjectConfig.java
+++ src/main/java/com/example/dao/config/InjectConfig.java
@@ -4,6 +4,7 @@ import org.seasar.doma.AnnotateWith;
 import org.seasar.doma.Annotation;
 import org.seasar.doma.AnnotationTarget;
 
+import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
@@ -11,8 +12,9 @@ import javax.inject.Inject;
  * Created by tada on 2015/10/12.
  */
 @AnnotateWith(annotations = {
-        @Annotation(target = AnnotationTarget.CLASS, type = Dependent.class)
-        , @Annotation(target = AnnotationTarget.CONSTRUCTOR, type = Inject.class)
+//        @Annotation(target = AnnotationTarget.CLASS, type = Dependent.class), // CDI
+        @Annotation(target = AnnotationTarget.CLASS, type = Stateless.class), // EJB
+        @Annotation(target = AnnotationTarget.CONSTRUCTOR, type = Inject.class)
 })
 public @interface InjectConfig {
 }
